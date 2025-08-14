0a44c06ce38ae97789f85209116786f0e6a5a068
Joseph Heck <j_heck@apple.com>
2025-08-14 09:47:35 -0700

    quickpass update, not complete (#18)
    * quickpass update, not complete
    
    * applying default swift format
    
    * using isEmpty in test
    
    * using a marker class to identify the bundle and load it for the macOS executable location

--- Sources/dealer/Dealer.swift
+++ Sources/dealer/Dealer.swift
@@ -1,67 +1,74 @@
-/*
- This source file is part of the Swift.org open source project
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the example package dealer open source project
+//
+// Copyright (c) 2015-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
- Copyright 2015 – 2021 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+import ArgumentParser
+import DeckOfPlayingCards
+import Foundation
+import PlayingCard
 
 #if os(Linux)
-import Glibc
+  import Glibc
 #endif
-
-import Foundation
-import DeckOfPlayingCards
-import PlayingCard
-import ArgumentParser
 
 @main
 struct Dealer: ParsableCommand {
-    enum Error: Swift.Error, CustomStringConvertible {
-        case notEnoughCards
+  enum Error: Swift.Error, CustomStringConvertible {
+    case notEnoughCards
 
-        var description: String {
-            switch self {
-            case .notEnoughCards:
-                return "Not enough cards"
-            }
-        }
+    var description: String {
+      switch self {
+      case .notEnoughCards:
+        return "Not enough cards"
+      }
     }
+  }
 
-    static var configuration = CommandConfiguration(
-            abstract: "Shuffles a deck of playing cards and deals a number of cards.",
-            discussion: """
-                For each count argument, prints a line of tab-delimited cards to stdout,
-                or if there aren't enough cards remaining,
-                prints "Not enough cards" to stderr and exits with a nonzero status.
-                """)
+  static let configuration = CommandConfiguration(
+    abstract: "Shuffles a deck of playing cards and deals a number of cards.",
+    discussion: """
+      For each count argument, prints a line of tab-delimited cards to stdout,
+      or if there aren't enough cards remaining,
+      prints "Not enough cards" to stderr and exits with a nonzero status.
+      """)
 
-    @Argument(help: .init("The number of cards to deal at a time.",
-                          valueName: "count"))
-    var counts: [UInt]
+  @Argument(
+    help: .init(
+      "The number of cards to deal at a time.",
+      valueName: "count"))
+  var counts: [UInt]
 
-    mutating func run() throws {
-        #if os(Linux)
-        srandom(UInt32(clock()))
-        #endif
+  mutating func run() throws {
+    #if os(Linux)
+      srandom(UInt32(clock()))
+    #endif
 
-        var deck = Deck.standard52CardDeck()
-        deck.shuffle()
+    var deck = Deck.standard52CardDeck()
+    deck.shuffle()
 
-        for count in counts {
-            var cards: [PlayingCard] = []
+    for count in counts {
+      var cards: [PlayingCard] = []
 
-            for _ in 0..<count {
-                guard let card = deck.deal() else {
-                    Self.exit(withError: Error.notEnoughCards)
-                }
-
-                cards.append(card)
-            }
-
-            print(cards.map(\.description).joined(separator: "\t"))
+      for _ in 0..<count {
+        guard let card = deck.deal() else {
+          Self.exit(withError: Error.notEnoughCards)
         }
+
+        cards.append(card)
+      }
+
+      print(cards.map(\.description).joined(separator: "\t"))
     }
+  }
 }
+
+// Empty class that provides a convenient handle to request
+// the bundle that contains this code.
+class BundleMarker {}
--- Tests/DealerTests/DealerTests.swift
+++ Tests/DealerTests/DealerTests.swift
@@ -1,103 +1,118 @@
-/*
- This source file is part of the Swift.org open source project
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the example package dealer open source project
+//
+// Copyright (c) 2021-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
- Copyright 2021 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
+import Foundation
+import Testing
 
- See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
-
-import XCTest
 import class Foundation.Bundle
 
-final class DealerTests: XCTestCase {
-    func testUsage() throws {
-        let (status, output, error) = try execute(with: ["--help"])
-        XCTAssertEqual(status, EXIT_SUCCESS)
-        XCTAssert(output?.starts(with: "OVERVIEW: Shuffles a deck of playing cards and deals a number of cards.") ?? false)
-        XCTAssertEqual(error, "")
-    }
+@testable import dealer
 
-    func testDealOneCard() throws {
-        let (status, output, error) = try execute(with: ["1"])
-        XCTAssertEqual(status, EXIT_SUCCESS)
-        XCTAssertEqual(output?.filter(\.isPlayingCardSuit).count, 1)
+struct DealerTests {
+  @Test
+  func testUsage() throws {
+    let (status, output, error) = try execute(with: ["--help"])
+    #expect(status == EXIT_SUCCESS)
+    #expect(
+      output?.starts(
+        with: "OVERVIEW: Shuffles a deck of playing cards and deals a number of cards.") ?? false)
+    #expect(error?.isEmpty == true)
+  }
 
-        XCTAssertEqual(error, "")
-    }
+  @Test
+  func testDealOneCard() throws {
+    let (status, output, error) = try execute(with: ["1"])
+    #expect(status == EXIT_SUCCESS)
+    #expect(output?.filter(\.isPlayingCardSuit).count == 1)
 
-    func testDealTenCards() throws {
-        let (status, output, error) = try execute(with: ["10"])
-        XCTAssertEqual(status, EXIT_SUCCESS)
-        XCTAssertEqual(output?.filter(\.isPlayingCardSuit).count, 10)
+    #expect(error?.isEmpty == true)
 
-        XCTAssertEqual(error, "")
-    }
+  }
 
-    func testDealThirteenCardsFourTimes() throws {
-        let (status, output, error) = try execute(with: ["13", "13", "13", "13"])
-        XCTAssertEqual(status, EXIT_SUCCESS)
-        XCTAssertEqual(output?.filter(\.isPlayingCardSuit).count, 52)
-        XCTAssertEqual(output?.filter(\.isNewline).count, 4)
+  @Test
+  func testDealTenCards() throws {
+    let (status, output, error) = try execute(with: ["10"])
+    #expect(status == EXIT_SUCCESS)
+    #expect(output?.filter(\.isPlayingCardSuit).count == 10)
 
-        XCTAssertEqual(error, "")
-    }
+    #expect(error?.isEmpty == true)
 
-    func testDealOneHundredCards() throws {
-        let (status, output, error) = try execute(with: ["100"])
-        XCTAssertNotEqual(status, EXIT_SUCCESS)
-        XCTAssertEqual(output, "")
-        XCTAssertEqual(error, "Error: Not enough cards\n")
-    }
+  }
 
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
-    }
+  @Test
+  func testDealThirteenCardsFourTimes() throws {
+    let (status, output, error) = try execute(with: ["13", "13", "13", "13"])
+    #expect(status == EXIT_SUCCESS)
+    #expect(output?.filter(\.isPlayingCardSuit).count == 52)
+    #expect(output?.filter(\.isNewline).count == 4)
 
-    private func execute(with arguments: [String] = []) throws -> (status: Int32, output: String?, error: String?) {
-        let process = Process()
-        process.executableURL = productsDirectory.appendingPathComponent("dealer")
-        process.arguments = arguments
+    #expect(error?.isEmpty == true)
 
-        let outputPipe = Pipe()
-        process.standardOutput = outputPipe
+  }
 
-        let errorPipe = Pipe()
-        process.standardError = errorPipe
+  @Test
+  func testDealOneHundredCards() throws {
+    let (status, output, error) = try execute(with: ["100"])
+    #expect(status != EXIT_SUCCESS)
+    #expect(output?.isEmpty == true)
 
-        try process.run()
-        process.waitUntilExit()
+    #expect(error == "Error: Not enough cards\n")
+  }
 
-        let status = process.terminationStatus
+  /// Returns path to the built products directory.
+  var productsDirectory: URL {
+    #if os(macOS)
+      return Bundle(for: BundleMarker.self).bundleURL.deletingLastPathComponent()
+    #else
+      return Bundle.main.bundleURL
+    #endif
+  }
 
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: outputData, encoding: .utf8)
+  private func execute(with arguments: [String] = []) throws -> (
+    status: Int32, output: String?, error: String?
+  ) {
+    let process = Process()
+    process.executableURL = productsDirectory.appendingPathComponent("dealer")
+    process.arguments = arguments
 
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        let error = String(data: errorData, encoding: .utf8)
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
 
-        return (status, output, error)
-    }
+    let errorPipe = Pipe()
+    process.standardError = errorPipe
+
+    try process.run()
+    process.waitUntilExit()
+
+    let status = process.terminationStatus
+
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: outputData, encoding: .utf8)
+
+    let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+    let error = String(data: errorData, encoding: .utf8)
+
+    return (status, output, error)
+  }
 }
 
 // MARK: -
 
-private extension Character {
-    var isPlayingCardSuit: Bool {
-        switch self {
-        case "♠︎", "♡", "♢", "♣︎":
-            return true
-        default:
-            return false
-        }
+extension Character {
+  fileprivate var isPlayingCardSuit: Bool {
+    switch self {
+    case "♠︎", "♡", "♢", "♣︎":
+      return true
+    default:
+      return false
     }
+  }
 }
